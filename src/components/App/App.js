db6859b2957cb0d60436e0f861c0add002273bc8
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-02-09 10:53:18 +0100

    updated

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import { Footer, GitRepositoryHeader} from '../common'
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <GitRepositoryHeader/>
+      <section className="main-section"/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/common/GitRepositoryHeader/GitRepositoryHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './GitRepositoryHeader.scss';
+
+function GitRepositoryHeader() {
+  return (<>
+    <header className="git-repository-header">Git repository 👉🏻<a href="https://github.com/lorenzoVwebDev/game-of-life">Game Of Life</a></header>
+  </>)
+}
+
+export default GitRepositoryHeader;
